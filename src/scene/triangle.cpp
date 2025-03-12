2fafe88dacb75cc7eeecc5d74111e1d3fdae6f64
Ruan Lixin <ruaneason@outlook.com>
2025-03-11 23:30:39 -0700

    Finished part 1 task3 and tested

--- src/scene/triangle.cpp
+++ src/scene/triangle.cpp
@@ -27,21 +27,50 @@ bool Triangle::has_intersection(const Ray &r) const {
   // The difference between this function and the next function is that the next
   // function records the "intersection" while this function only tests whether
   // there is a intersection.
-
-
+  // use Moller Trumbore Algorithm
+  const auto e1 = p2 - p1;
+  const auto e2 = p3 - p1;
+  const auto s = r.o - p1;
+  const auto s1 = cross(r.d, e2);
+  const auto s2 = cross(s, e1);
+  const auto t =  dot(s2, e2) / dot(s1, e1);
+  if (t < r.min_t || t > r.max_t) {return false;}
+  const auto b1 = dot(s1, s) / dot(s1, e1);
+  if (b1 < 0.0 || b1 > 1.0) {return false;}
+  const auto b2 =dot(s2, r.d) / dot(s1, e1);
+  if (b2 < 0.0 || b2 > 1.0) {return false;}
+  const auto b3 = 1. - b1 - b2;
+  if (b3 < 0.0 || b3 > 1.0) {return false;}
+  // proved to be intersected here
+  r.max_t = t;
   return true;
-
 }
 
 bool Triangle::intersect(const Ray &r, Intersection *isect) const {
   // Part 1, Task 3:
   // implement ray-triangle intersection. When an intersection takes
   // place, the Intersection data should be updated accordingly
-
-
+  // use Moller Trumbore Algorithm
+  const auto e1 = p2 - p1;
+  const auto e2 = p3 - p1;
+  const auto s = r.o - p1;
+  const auto s1 = cross(r.d, e2);
+  const auto s2 = cross(s, e1);
+  const auto t =  dot(s2, e2) / dot(s1, e1);
+  if (t < r.min_t || t > r.max_t) {return false;}
+  const auto b1 = dot(s1, s) / dot(s1, e1);
+  if (b1 < 0.0 || b1 > 1.0) {return false;}
+  const auto b2 =dot(s2, r.d) / dot(s1, e1);
+  if (b2 < 0.0 || b2 > 1.0) {return false;}
+  const auto b0 = 1. - b1 - b2;
+  if (b0 < 0.0 || b0 > 1.0) {return false;}
+  // proved to be intersected here
+  r.max_t = t;
+  isect->t = t;
+  isect->n =  b0 * n1 + b1 * n2 + b2 * n3;
+  isect->primitive = this;
+  isect->bsdf = get_bsdf();
   return true;
-
-
 }
 
 void Triangle::draw(const Color &c, float alpha) const {
