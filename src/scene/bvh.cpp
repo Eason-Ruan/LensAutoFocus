5627ebba01ad8d72c4c24cc2cb94d3ada91eb4db
Ruan Lixin <ruaneason@outlook.com>
2025-03-17 01:07:03 -0700

    Finsh and tested part 2 task1 with centroids and hand swap

--- src/scene/bvh.cpp
+++ src/scene/bvh.cpp
@@ -57,22 +57,112 @@ BVHNode *BVHAccel::construct_bvh(std::vector<Primitive *>::iterator start,
   // size configuration. The starter code build a BVH aggregate with a
   // single leaf node (which is also the root) that encloses all the
   // primitives.
-
-
   BBox bbox;
-
-  for (auto p = start; p != end; p++) {
+  auto tmp_l = start;
+  auto tmp_r = end;
+  for (auto p = start; p != end; ++p) {
     BBox bb = (*p)->get_bbox();
     bbox.expand(bb);
   }
-
-  BVHNode *node = new BVHNode(bbox);
-  node->start = start;
-  node->end = end;
-
+  auto *node = new BVHNode(bbox);
+  const int length = static_cast<int>(end - start);
+  if (length > max_leaf_size) {
+    // determine axes
+    const double x_center = bbox.centroid().x;
+    const double y_center = bbox.centroid().y;
+    const double z_center = bbox.centroid().z;
+    BBox x_left, x_right, y_left, y_right, z_left, z_right;
+    int x_left_count = 0;
+    int x_right_count = 0;
+    int y_left_count = 0;
+    int y_right_count = 0;
+    int z_left_count = 0;
+    int z_right_count = 0;
+    for (auto p = start; p != end; ++p) {
+      const BBox bb = (*p)->get_bbox();
+      const Vector3D center = bb.centroid();
+      if (center.x <= x_center) {
+        x_left.expand(bb);
+        x_left_count++;
+      } else {
+        x_right.expand(bb);
+        x_right_count++;
+      }
+      if (center.y <= y_center) {
+        y_left.expand(bb);
+        y_left_count++;
+      } else {
+        y_right.expand(bb);
+        y_right_count++;
+      }
+      if (center.z <= z_center) {
+        z_left.expand(bb);
+        z_left_count++;
+      } else {
+        z_right.expand(bb);
+        z_right_count++;
+      }
+    }
+    double cost_x = (x_left.surface_area()* x_left_count
+    + x_right.surface_area() * x_right_count) / bbox.surface_area();
+    double cost_y = (y_left.surface_area() * y_left_count + y_right.surface_area() * y_right_count) / bbox.surface_area();
+    double cost_z = (z_left.surface_area() * z_left_count + z_right.surface_area() * z_right_count) / bbox.surface_area();
+    if (cost_x < cost_y && cost_x < cost_z) {
+      while (tmp_l < tmp_r) {
+        if ((*tmp_l)->get_bbox().centroid().x > x_center) {
+          do {
+            --tmp_r;
+          } while (!((*tmp_r)->get_bbox().centroid().x < x_center) && tmp_l < tmp_r);
+          if (tmp_l < tmp_r) {
+            iter_swap(tmp_l, tmp_r);
+          }else {break;}
+        }else {
+          ++ tmp_l;
+        }
+      }
+      if (tmp_r == end || tmp_l == start) {
+        tmp_l = start + (end - start) / 2;
+      }
+    }else if  (cost_y < cost_x && cost_y < cost_z) {
+      while (tmp_l < tmp_r) {
+        if ((*tmp_l)->get_bbox().centroid().y > y_center) {
+          do {
+            --tmp_r;
+          } while (!((*tmp_r)->get_bbox().centroid().y < y_center) && tmp_l < tmp_r);
+          if (tmp_l < tmp_r) {
+            iter_swap(tmp_l, tmp_r);
+          }else {break;}
+        }else {
+          ++ tmp_l;
+        }
+      }
+      if (tmp_r == end || tmp_l == start) {
+        tmp_l = start + (end - start) / 2;
+      }
+    }else {
+      while (tmp_l < tmp_r) {
+        if ((*tmp_l)->get_bbox().centroid().z > z_center) {
+          do {
+            --tmp_r;
+          } while (!((*tmp_r)->get_bbox().centroid().z < z_center) && tmp_l < tmp_r);
+          if (tmp_l < tmp_r) {
+            iter_swap(tmp_l, tmp_r);
+          }else {break;}
+        }else {
+          ++ tmp_l;
+        }
+      }
+      if (tmp_r == end || tmp_l == start) {
+        tmp_l = start + (end - start) / 2;
+      }
+    }
+    node->l = construct_bvh(start, tmp_l, max_leaf_size);
+    node->r = construct_bvh(tmp_l, end, max_leaf_size);
+  } else {
+    node->start = start;
+    node->end = end;
+  }
   return node;
-
-
 }
 
 bool BVHAccel::has_intersection(const Ray &ray, BVHNode *node) const {
